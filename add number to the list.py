fdad3d5c2811d9a819961813f10414d08419fbf4
unknown <azza96alkiuymi@gmail.com>
2023-11-21 14:07:03 +0400

    list

---
+++ add number to the list.py
@@ -0,0 +1,9 @@
+number=[]
+addnumber=int(input("Enter your number "))
+
+while (addnumber != 'Q'):
+    i=number.append(addnumber)
+    addnumber=input("Enter your number ")
+else:
+    print("stop")
+    print(number)
---
+++ position.py
@@ -0,0 +1,8 @@
+x=[100 , 201 , 99 , 101]
+
+for i in x:
+    if(i>100):
+        print(i)
+        print("position" ,x.index(i))
+else:
+    print(" ")
